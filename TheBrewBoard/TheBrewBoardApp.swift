ae280c1cc0be7437cf2007f28c7cdd2fe01b2545
paolo-bae <paolo.baeli@gmail.com>
2023-01-19 15:45:53 +0100

    Initial Commit

---
+++ TheBrewBoard/TheBrewBoardApp.swift
@@ -0,0 +1,20 @@
+//
+//  TheBrewBoardApp.swift
+//  TheBrewBoard
+//
+//  Created by Paolo Baeli on 19/01/23.
+//
+
+import SwiftUI
+
+@main
+struct TheBrewBoardApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
